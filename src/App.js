04cd503ed31b0c2b744555b57e0b3c9e45f6f9de
Hesham Alkurdi <alkurdi.hesham@gmail.com>
2019-04-23 11:34:52 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import con4 from './connect4'
 
 class App extends Component {
+  
   render() {
+    let contstyle = {
+      width:"600px",
+      height:"500px",
+      backgroundColor:"red",
+      margin:"auto"
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container" style={contstyle}>
+
+        </div>
       </div>
     );
   }
