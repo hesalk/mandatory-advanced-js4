2c661854c264f90e94ec36c3fc7231c7ade83238
Hesham Alkurdi <alkurdi.hesham@gmail.com>
2019-04-24 10:38:13 +0200

    need to change the row

--- src/connect4.js
+++ src/connect4.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect,useReducer  } from "react";
 
+function testco(arr){
+    console.log(arr);
+    arr[1].props.function()
+    
+}
+
+
 function con4() {
-    const [color,updatecolor] = useState("white")
-    const newColor = color === "red" ? "blue" : "red";
+
+    let testcolot = (array)=> {
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            console.log(element.props.style.backgroundColor)
+            if(element.props.style.backgroundColor === "white"){
+                console.log("xx");
+                return "red"
+            }
+        }
+        
+    }
+    let test = (s)=>{
+        if(s === "white"){
+            return "red"
+        }
+        else{return "blue"}
+    }
     let colstyle = {
     }
     let con4style = {
@@ -12,29 +35,33 @@ function con4() {
         flexWrap:"wrap",
         flexDirection: "column-reverse"
     }
-    let sqrstyle = {
-        width:"90px",
-        height:"90px",
-        backgroundColor:"white",
-        margin:"5px",
-        backgroundColor:color
-    }
+
     let row = new Array(42)
+
     for (let i = 0; i < row.length; i++) {
+        const [color,updatecolor] = useState("white")
+        let sqrstyle = {
+            width:"90px",
+            height:"90px",
+            backgroundColor:"white",
+            margin:"5px",
+            backgroundColor:color
+        }
         const element = row[i];
-        row[i]= <div key={i}  style={sqrstyle}></div>
+        row[i]= <div key={i} function={()=>{updatecolor(test(color))}} onClick={console.log("clicked")} style={sqrstyle}></div>
     }
 
+    console.log(row[1].key)
     console.log(row)
     return(
         <div className="con4" style={con4style} >
-            <div onClick={()=>{updatecolor(newColor)}} style={colstyle} className="col-1">{row.slice(0,6)}</div>
-            <div  className="col-2">{row.slice(6,12)}</div>
+            <div onClick={()=>{testco(row.slice(0,6))}} style={colstyle} className="col-1">{row.slice(0,6)}</div>
+            <div onClick={()=>{}} className="col-2">{row.slice(6,12)}</div>
             <div  className="col-3">{row.slice(12,18)}</div>
             <div  className="col-4">{row.slice(18,24)}</div>
             <div  className="col-5">{row.slice(24,30)}</div>
             <div  className="col-6">{row.slice(30,36)}</div>
-            <div  className="col-6">{row.slice(36,42)}</div>
+            <div  className="col-7">{row.slice(36,42)}</div>
         </div>
     )
 }
